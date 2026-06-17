41a1e44c80fc32f560782547181b28113bf0c416
MrSkyAngule <garhok124@gmail.com>
2026-06-17 16:46:28 +0300

    first commit

--- ois/settings.py
+++ ois/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ois.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HOLIK',
+        'USER': 'owner',
+        'PASSWORD': 'GoodGod',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
