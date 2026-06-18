18412db333fabdb01e8106e3aea43ab078b8d21c
MrSkyAngule <garhok124@gmail.com>
2026-06-18 23:30:04 +0300

    first commit

--- about/views.py
+++ about/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request, 'about.html')
+    context = {
+        'project_name': 'Моя Инженерная Фабрика',
+        'project_description': 'Мы строим автоматические заводы на базе андезитовых сплавов...',
+        'project_goals': 'Построить самую быструю линию сборки предметов.',
+        'version': 'v2.4',
+        'automation_rate': '85%',
+        'speed_rpm': '128 RPM'
+    }
+    return render(request, 'about.html', context)
